cf069aa7099f92592394f54c777a884b8d083977
David-ruiz015 <116306695+David-ruiz015@users.noreply.github.com>
2024-01-21 16:37:24 -0500

    repo-ima-04

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import PropTypes from 'prop-types';
+import {
+  SearchForm,
+  SearchInput,
+  SearchButton,
+  SearchSpan,
+  SearchLogo,
+} from './SearchBar.styled';
+
+const SearchBar = ({ onSubmit }) => {
+  const [searchName, setSearchName] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = event => setInputValue(event.target.value);
+
+  const handleSubmit = event => {
+    event.preventDefault(); 
+    setSearchName(inputValue.trim()); 
+    onSubmit(searchName); 
+    event.target.reset(); 
+  };
+
+  return (
+    <header>
+      <SearchForm onSubmit={handleSubmit}>
+        <a href="https://pixabay.com/" target="_blank" rel="noreferrer">
+          <SearchLogo
+            src={require('./pixabay-logo.png')} 
+            alt="logo"
+            width="200"
+          />
+        </a>
+        <SearchButton>
+          <BsSearch />
+          <SearchSpan>Search</SearchSpan>
+        </SearchButton>
+        <SearchInput
+          name="searchName"
+          type="text"
+          id="search"
+          value={inputValue}
+          onChange={handleChange}
+        />
+      </SearchForm>
+    </header>
+  );
+};
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
